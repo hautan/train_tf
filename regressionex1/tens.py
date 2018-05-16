3bc39ae89d7899ffeee303f3bd8a651be907844c
hautan <floringavrila@gmail.com>
2018-05-17 00:10:46 +0300

    add first regression example

---
+++ regressionex1/tens.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+ # We must always import the relevant libraries for our problem at hand. NumPy and TensorFlow are required for this example.
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+batch_size = 8
+
