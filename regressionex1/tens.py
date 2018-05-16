27c37c72a5b1959c15800794f33f3fad092a6c0b
hautan <floringavrila@gmail.com>
2018-05-17 00:12:16 +0300

    saved first ex changes

--- regressionex1/tens.py
+++ regressionex1/tens.py
@@ -4,5 +4,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+x_data = np.linspace(0.0, 10.0, 1000000)
+y_data = (0.5*x_data) + 5
+
 batch_size = 8
+
+w = tf.Variable(0.5)
+b = tf.Variable(0.5)
+
+xph = tf.placeholder(tf.float32, [batch_size])
+yph = tf.placeholder(tf.float32, [batch_size])
+
+y_model = w*xph + b
+
+error = tf.reduce_sum(tf.square(yph-y_model))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+train = optimizer.minimize(error)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+  sess.run(init)
+  batches = 10000
+  for i in range(batches):
+    rand_ind = np.random.randint(len(x_data),size=batch_size)
+    print(rand_ind)
+    feed = {xph:x_data[rand_ind], yph:y_data[rand_ind]}
+    sess.run(train, feed_dict=feed)
+  model_w, model_b = sess.run([w,b])
+
 
